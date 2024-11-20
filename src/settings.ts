cdb3305be3e25005c2719002c23b045e1c700c0c
JP Sheehan <sheehan_john@bah.com>
2024-11-20 12:12:55 -0500

    Parse json

--- src/settings.ts
+++ src/settings.ts
@@ -1,46 +1,4 @@
-// import { assert } from "console";
-
-
-// type Value = any;
-
-// type SettingsTree = {
-//     key: string | null,
-//     value: SettingsTree[] | Value,
-// };
-
 export function parseFile(text: string): number {
-    return text.length;
+    let obj = JSON.parse(text);
+    return Object.keys(obj).length;
 }
-
-// function parseSetting(text: string): [SettingsTree, string] {
-//     text = text.trim();
-
-//     const key = parseKey(text);
-//     text = text.slice(key.length).trim();
-
-//     assert(text.startsWith(':'));
-//     text = text.slice(1).trim();
-
-//     const [value, rest] = parseValue(text);
-
-//     return [{ key, value }, rest];
-// }
-
-// function parseKey(text: string): string | null {
-//     assert(text.startsWith('"'));
-
-//     const key = text.match(/^"([^"]+)"/);
-
-//     if (!key) {
-//         return null;
-//     }
-
-//     return key[1];
-// }
-
-// function parseValue(text: string): [SettingsTree[]] {
-//     text = text.trim();
-
-//     // parse json
-
-// }
