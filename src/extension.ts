160e904470d606fd0c2b60ec9eefa261b44cbad3
JP Sheehan <sheehan_john@bah.com>
2024-11-20 12:55:45 -0500

    Run organizer

--- src/extension.ts
+++ src/extension.ts
@@ -14,12 +14,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('settings-organizer.characterCount', () => {
+	let disposable = vscode.commands.registerCommand('settings-organizer.organize', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		const file = vscode.window.activeTextEditor?.document.getText();
-		const count = organizeSettings(file || '', 4).length;
-		vscode.window.showInformationMessage(`Character count: ${count}`);
+		// Default spacing of JSON files
+		const workbenchConfig = vscode.workspace.getConfiguration('workbench');
+		const spacing = workbenchConfig.get<number>('editor.tabSize');
+		const defaultSpacing = 4;
+		const organizedSettings = organizeSettings(file || '', spacing || defaultSpacing);
+		// Replace contents of file
+		vscode.window.activeTextEditor?.edit(editBuilder => {
+			editBuilder.replace(
+				new vscode.Range(new vscode.Position(0, 0),
+				new vscode.Position(vscode.window.activeTextEditor?.document.lineCount || 0, 0)),
+				organizedSettings);
+		});
 	});
 
 	context.subscriptions.push(disposable);
