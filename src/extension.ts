54c7c971ac87b090d94375d32585cd6614ffc9c9
JP Sheehan <sheehan_john@bah.com>
2024-11-20 13:27:46 -0500

    Use Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -6,33 +6,46 @@ import { organizeSettings } from './settings';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "settings-organizer" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "settings-organizer" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'settings-organizer.organize',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const file = vscode.window.activeTextEditor?.document.getText();
+      // Default spacing of JSON files
+      const workbenchConfig = vscode.workspace.getConfiguration('workbench');
+      const spacing = workbenchConfig.get<number>('editor.tabSize');
+      const defaultSpacing = 4;
+      const organizedSettings = organizeSettings(
+        file || '',
+        spacing || defaultSpacing
+      );
+      // Replace contents of file
+      vscode.window.activeTextEditor?.edit((editBuilder) => {
+        editBuilder.replace(
+          new vscode.Range(
+            new vscode.Position(0, 0),
+            new vscode.Position(
+              vscode.window.activeTextEditor?.document.lineCount || 0,
+              0
+            )
+          ),
+          organizedSettings
+        );
+      });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('settings-organizer.organize', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const file = vscode.window.activeTextEditor?.document.getText();
-		// Default spacing of JSON files
-		const workbenchConfig = vscode.workspace.getConfiguration('workbench');
-		const spacing = workbenchConfig.get<number>('editor.tabSize');
-		const defaultSpacing = 4;
-		const organizedSettings = organizeSettings(file || '', spacing || defaultSpacing);
-		// Replace contents of file
-		vscode.window.activeTextEditor?.edit(editBuilder => {
-			editBuilder.replace(
-				new vscode.Range(new vscode.Position(0, 0),
-				new vscode.Position(vscode.window.activeTextEditor?.document.lineCount || 0, 0)),
-				organizedSettings);
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
--- src/settings.ts
+++ src/settings.ts
@@ -1,81 +1,81 @@
 import JSON5 from 'json5';
 
-interface Settings { [key: string]: any }
-
-export function organizeSettings(text: string, space: number): string {
-    let settings = JSON5.parse(text);
-    settings = decompose(settings);
-    settings = condense(settings);
-    settings = sortKeys(settings);
-    return JSON.stringify(settings, null, space);
+interface Settings {
+  [key: string]: any;
 }
 
+export function organizeSettings(text: string, space: number): string {
+  let settings = JSON5.parse(text);
+  settings = decompose(settings);
+  settings = condense(settings);
+  settings = sortKeys(settings);
+  return JSON.stringify(settings, null, space);
+}
 
 export function decompose(settings: Settings): Settings {
-    let result: Settings = {};
-    for (let key in settings) {
+  let result: Settings = {};
+  for (let key in settings) {
+    const subkeys = key.split('.');
+    const value = settings[key];
 
-        const subkeys = key.split('.');
-        const value = settings[key];
-
-        if (subkeys.length > 1) {
-            const topKey = subkeys[0];
-            const subValue = {[subkeys.slice(1).join('.')]: value};
-            if (topKey in result) {
-                let joinedSettings = {...result[topKey], ...subValue};
-                result[topKey] = decompose(joinedSettings);
-            } else {
-                result[topKey] = decompose(subValue);
-            }
-        } else {
-            if (typeof value === 'object' && !Array.isArray(value)) {
-                result[key] = decompose(value);
-            } else {
-                result[key] = value;
-            }
-        }
+    if (subkeys.length > 1) {
+      const topKey = subkeys[0];
+      const subValue = { [subkeys.slice(1).join('.')]: value };
+      if (topKey in result) {
+        let joinedSettings = { ...result[topKey], ...subValue };
+        result[topKey] = decompose(joinedSettings);
+      } else {
+        result[topKey] = decompose(subValue);
+      }
+    } else {
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        result[key] = decompose(value);
+      } else {
+        result[key] = value;
+      }
     }
-    return result;
+  }
+  return result;
 }
 
 export function condense(settings: Settings): Settings {
-    let result: Settings = {};
-    for (let key in settings) {
-        const value = settings[key];
-        if (key.startsWith('[')) {
-            result[key] = condense(value);
-        } else if (typeof value === 'object' && !Array.isArray(value)) {
-            const keys = Object.keys(value);
-            if (keys.length === 1) {
-                const subkey = keys[0];
-                result[`${key}.${subkey}`] = value[subkey];
-            } else {
-                result[key] = condense(value);
-            }
-        } else {
-            result[key] = value;
-        }
+  let result: Settings = {};
+  for (let key in settings) {
+    const value = settings[key];
+    if (key.startsWith('[')) {
+      result[key] = condense(value);
+    } else if (typeof value === 'object' && !Array.isArray(value)) {
+      const keys = Object.keys(value);
+      if (keys.length === 1) {
+        const subkey = keys[0];
+        result[`${key}.${subkey}`] = value[subkey];
+      } else {
+        result[key] = condense(value);
+      }
+    } else {
+      result[key] = value;
     }
-    return result;
+  }
+  return result;
 }
 
 export function sortKeys(settings: Settings): Settings {
-    let result: Settings = {};
-    for (let key of Object.keys(settings).sort(compare)) {
-        const value = settings[key];
-        if (typeof value === 'object' && !Array.isArray(value)) {
-            result[key] = sortKeys(value);
-        } else {
-            result[key] = value;
-        }
+  let result: Settings = {};
+  for (let key of Object.keys(settings).sort(compare)) {
+    const value = settings[key];
+    if (typeof value === 'object' && !Array.isArray(value)) {
+      result[key] = sortKeys(value);
+    } else {
+      result[key] = value;
     }
-    return result;
+  }
+  return result;
 }
 
 function compare(a: string, b: string): number {
-    // Sort alphabetically, putting keys in brackets last
-    if (a.startsWith('[') !== b.startsWith('[')) {
-        return a.startsWith('[') ? 1 : -1;
-    }
-    return a.localeCompare(b);
+  // Sort alphabetically, putting keys in brackets last
+  if (a.startsWith('[') !== b.startsWith('[')) {
+    return a.startsWith('[') ? 1 : -1;
+  }
+  return a.localeCompare(b);
 }
